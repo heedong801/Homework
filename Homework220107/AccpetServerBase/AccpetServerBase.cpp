bfa6f66fc57105482e37c0fe6a19fc6632816f5f
heedong <hidong801@naver.com>
2022-01-07 15:55:59 +0900

    First Test

---
+++ Homework220107/AccpetServerBase/AccpetServerBase.cpp
@@ -0,0 +1,130 @@
+﻿// AccpetServerBase.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <conio.h>
+
+// #include <WinSock2.h> 윈도우에서는 서버통신을 하려면 이녀석을 사용해야 한다.
+// 윈도우에서 소켓통신에 관련된 함수와 구조체들을 모아 놓은 헤더
+// 주의사항
+// window와 충돌이 날수가 있어서 언제나 
+// window.h를 이 헤더 아래쪽에 놔야 한다.
+#include <WinSock2.h>
+#include <Windows.h> // 아래쪽에 놔야 합니다.
+#include <WS2tcpip.h> // inet_pton 함수를 사용하기 위한 헤더
+
+#pragma comment (lib, "ws2_32") // 서버통신용 구현이 들어있는 라이브러리
+
+int main()
+{
+	// 1
+	
+	// 00000001 00000000 00000000 00000000
+
+	WSADATA wsa;
+
+	// 윈도우에서 우리 프로그램이 소켓통신을 하겠다고 미리 알려주는 함수
+	// MAKEWORD(2, 2) 소켓 통신 방식 버전을 알려준다.
+	// 대부분 2.2 버전이기 때문에 아래와 같이 입력해주면 된다.
+	if (0 != WSAStartup(MAKEWORD(2, 2), &wsa))
+	{
+		std::cout << "WSAStartup Error" << std::endl;
+	}
+
+	// 소켓통신을 위한 주소정보를 담는 구조체
+	SOCKADDR_IN Add = { 0, };
+	Add.sin_family = AF_INET; // ip버전 4로 주소체계를 잡겠다.
+
+	// port는
+	// ip주소로 내 컴퓨터까지는 올수 있다.
+	// 내 컴퓨터까지 와서
+	// 여러개의 프로그램들이 뭔가 통신을 하고 있다면
+	// 그중에서 어디로 게임패킷을 보내야 하지?
+	// 온라인 게임 3개 켜놨으면 그중 어디로 가야하지?
+	// 특정 번호를 정해서 패킷이 도착할 위치를 정할수 있다.
+	Add.sin_port = htons(30001);
+
+	// 리틀에디안 빅에디안을 몰라도 됩니다.
+	// 주소값을 각 컴퓨터 사양과 칩셋 사양에 맞게 알아서 만들어 줍니다.
+
+	// "127.0.0.1"
+
+	if (SOCKET_ERROR == inet_pton(AF_INET, "0.0.0.0", &Add.sin_addr))
+	{
+		return 0;
+	}
+
+	std::cout << "server start" << std::endl;
+
+	// SOCK_STREAM 연결지속을 하겠다는 옵션
+	SOCKET ServerAccpetSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (INVALID_SOCKET == ServerAccpetSocket)
+	{
+		return 0;
+	}
+
+
+	if (SOCKET_ERROR == bind(ServerAccpetSocket, (const sockaddr*)&Add, sizeof(SOCKADDR_IN)))
+	{
+		return 0;
+	}
+
+
+	// 동시에 접속요청이 오면 몇명까지 처리할거냐?
+
+	if (SOCKET_ERROR == listen(ServerAccpetSocket, 512))
+	{
+		return 0;
+	}
+
+	SOCKADDR_IN UserAdd = { 0, };
+	int Len = sizeof(SOCKADDR_IN);
+	// 접속을 기다리겠다.
+
+	// 이게 리턴되면 기본적으로 2가지 경우중 하나
+	// 유저가 진짜 접속했다.
+	// 프로그램에 문제가 생겼다.
+	std::cout << "서버 접속 함수 실행" << std::endl;
+
+	SOCKET SessionSocket = accept(ServerAccpetSocket, (sockaddr*)&UserAdd, &Len);
+
+	// 바이트를 보내는데
+	// 필요한 모든 것들을 프로토콜이라고 부르는게 맞느냐.
+
+	// "<HEAD>"
+
+
+	// "https://www.google.com/search?q=%EC%9C%A0%ED%8A%9C%EB%B8%8C&oq=%EC%9C%A0%ED%8A%9C%EB%B8%8C&aqs=chrome.0.0i131i355i433i512j46i131i199i433i465i512j0i512j0i433i512j0i512j0i433i512j0i131i433i512j0i433i512j0i512l2.1737j0j4&sourceid=chrome&ie=UTF-8"
+	// 브라우저?
+
+	// 클라의 184 소켓은 <=> 서버의 196 소켓과
+
+	// 서버에서는 바이트를 날리고 바이트를 받는게 전부인데.
+	// 그 바이트를 날리는 빈도 수준 방법 프로토콜
+
+	// 10바이트를 보내는데 무조건 앞에 4바이트에는 이게 저 사람과 몇번째 패킷인지 보낸다.
+
+	if (INVALID_SOCKET == SessionSocket)
+	{
+		return 0;
+	}
+
+	std::cout << "패킷 보내기 전 대기" << std::endl;
+	_getch();
+
+	int Result = send(SessionSocket, "hi", sizeof("hi"), 0);
+
+	if (SOCKET_ERROR == Result)
+	{
+		return 0;
+	}
+
+	// 동기 리시브를 쓴다는게 말이 안된다.
+	// recv(SessionSocket, "hi", sizeof("hi"), 0);
+
+	std::cout << "접속자가 있습니다" << std::endl;
+
+	_getch();
+
+}
---
+++ Homework220107/ConnectClientBase/ConnectClientBase.cpp
@@ -0,0 +1,189 @@
+﻿// ConnectClientBase.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <conio.h>
+
+#include <WinSock2.h>
+#include <Windows.h> // 아래쪽에 놔야 합니다.
+#include <WS2tcpip.h> // inet_pton 함수를 사용하기 위한 헤더
+#include <cctype>
+#include <string>
+#pragma comment (lib, "ws2_32") // 서버통신용 구현이 들어있는 라이브러리
+
+
+int main()
+{
+	WSADATA wsa;
+
+	// 윈도우에서 우리 프로그램이 소켓통신을 하겠다고 미리 알려주는 함수
+	// MAKEWORD(2, 2) 소켓 통신 방식 버전을 알려준다.
+	// 대부분 2.2 버전이기 때문에 아래와 같이 입력해주면 된다.
+	if (0 != WSAStartup(MAKEWORD(2, 2), &wsa))
+	{
+		std::cout << "WSAStartup Error" << std::endl;
+	}
+
+
+	// 소켓통신을 위한 주소정보를 담는 구조체
+
+	//         연결지속           종료
+	// 프로토콜  SYN ACK
+	// TCP 3 Way-Handshake & 4 Way-Handshake
+	SOCKET SessionSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// 릴레이어블 UDP 
+	// UDP 보냈어 상대인 서버가 받든 안받는 그건 상관 없는일.
+	// SOCKET ServerConnectSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (INVALID_SOCKET == SessionSocket)
+	{
+		return 0;
+	}
+
+	// 클라는 바인드를 할필요가 없습니다.
+
+	//std::cout << "접속 준비 키를 누르면 접속합니다" << std::endl;
+	//_getch();
+
+
+	std::string Ip;
+	std::string Port;
+	bool resetFlag = false;
+	while (true)
+	{
+		std::cout << "IP주소를 입력해주세요" << std::endl;
+		std::cin >> Ip;
+
+		if ("Q" == Ip || "q" == Ip)
+		{
+			Ip = "127.0.0.1";
+		}
+
+		//ipv6 는 : 2개이상 or :: 연속 등장 or 영문자로 판별 가능
+		char DotDelimiter = '.';
+		char Delimiter = ':';
+		int DelimiterCnt = 0;
+		int DotDelimiterCnt = 0;
+		std::string numStr = "";
+		for (int i = 0; i < Ip.length(); ++i)
+		{
+			if (Delimiter == Ip[i])
+			{
+				DelimiterCnt++;
+				if (DelimiterCnt >= 2)
+				{
+					resetFlag = true;
+					break;
+				}
+			}
+			else if(DotDelimiter == Ip[i])
+			{
+				DotDelimiterCnt++;
+				int num = stoi(numStr);
+				numStr = "";
+				if( num >= 256)
+				{
+					resetFlag = true;
+					break;
+				}
+			}
+			else if (isdigit(Ip[i]) == 0 ) // 영문자
+			{
+				resetFlag = true;
+				break;
+			}
+			else if (isdigit(Ip[i]) != 0)
+			{
+				numStr += Ip[i];
+			}
+			
+		}
+
+		if (DotDelimiterCnt > 3)
+			resetFlag = true;
+		else if (DotDelimiterCnt < 3)
+			resetFlag = true;
+
+
+		if (resetFlag)
+			continue;
+		/////////////////////////////////////////////////////////
+
+		//포트번호 입력 되어있나?
+		size_t findIdx = Ip.find(':');
+		if (findIdx != std::string::npos)
+		{
+			Port = Ip.substr(findIdx + 1);
+		}
+		else
+		{
+			std::cout << "포트번호를 입력해주세요" << std::endl;
+			std::cin >> Port;
+		}
+		Ip = Ip.substr(0, findIdx);
+		// if(포트번호를 안넣었다면)
+		// {
+		//   std::cout << "포트 번호를 입력해주세요" << std::endl;
+		// }
+
+		// IP 주소 4가 확실하다면
+		// 여기를 이탈
+
+		break;
+	}
+
+	int a = 0;
+
+
+	SOCKADDR_IN Add = { 0, };
+	Add.sin_family = AF_INET;
+	Add.sin_port = htons(stoi(Port));
+	if (SOCKET_ERROR == inet_pton(AF_INET, Ip.c_str(), &Add.sin_addr))
+	{
+		return 0;
+	}
+	int Len = sizeof(SOCKADDR_IN);
+
+	// 세션(session)은 컴퓨터 과학에서, 
+	// 특히 네트워크 분야에서 반영구적이고 상호작용적인 
+	// 정보 교환을 전제하는 둘 이상의 통신 장치나 컴퓨터와 
+	// 사용자 간의 대화나 송수신 연결상태를 의미하는 보안적인 
+	// 다이얼로그(dialogue) 및 시간대를 가리킨다. 
+	// 따라서 세션은 연결상태를 유지하는것보다 
+	// 연결상태의 안정성을 더 중요시 하게된다.
+
+
+
+	if (SOCKET_ERROR == connect(SessionSocket, (const sockaddr*)&Add, Len))
+	{
+		return 0;
+	}
+
+	char Buffer[1024] = {0};
+
+	// 어떤 특정상황이 올때까지 기다려주는 함수를
+	// 동기함수
+
+	// 비동기는 
+
+	
+	// SYNC         ASYNC
+	// 동기리시브와 비동기 
+	// 기다리는것도 포함한다.
+	int Result = recv(SessionSocket, Buffer, sizeof(Buffer), 0);
+
+	if (SOCKET_ERROR == Result)
+	{
+		return 0;
+	}
+
+
+	// 이 소켓은 이제 서버와 연결된거에요.
+	// ServerConnectSocket
+
+	std::cout << "접속에 성공했습니다." << std::endl;
+
+	_getch();
+
+}
