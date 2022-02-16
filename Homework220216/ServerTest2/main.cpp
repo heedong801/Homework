83dcb1b8f2a1e97952ddcdb1b58bada3bd3c109b
heedong <hidong801@naver.com>
2022-02-16 17:43:08 +0900

    Test Commit

--- Homework220216/ServerTest2/main.cpp
+++ Homework220216/ServerTest2/main.cpp
@@ -7,6 +7,7 @@ class String
 {
 public:
 
+    // 큰수가 들어와도 다 바뀌었으면 끝, 음수가 들어오면 변하지 않고 끝, 찾으려는 문자열이 없으면 그냥 그대로
     static void Replace(std::string& _Text, const std::string& _Prev, const std::string& _Next, int _Count = 1)
     {
         int cnt = 0;
@@ -20,46 +21,37 @@ public:
         return;
     }
 
-    // "ffasdfsdafdas" "cccc"
+    // 삽입하려는 인덱스가 문자열 길이보다 클 경우 삽입 x, 인덱스가 0보다 작을경우 삽입 x
     static void Insert(std::string& _Text, int _Index, const std::string& _InsertText)
     {
-        if (_Text.length() < _Index)
+        if (_Text.length() < _Index || _Index < 0)
             return;
         _Text.insert(_Index, _InsertText);
     }
 
-    // ' '
+    // 공백 제거
     static void TrimRemove(std::string& _Text)
     {
-        Replace(_Text, " ", "", _Text.length() - 1);
+        Replace(_Text, " ", "", static_cast<int>(_Text.length() - 1));
     }
 
-    // 한글이 섞여있어도.
     static void ToUpper(std::string& _Text)
     {
         std::transform(_Text.begin(), _Text.end(), _Text.begin(), [](const char& c) { return std::toupper(c); });
     }
 
-    // ReMove
+    // 전부 삭제
     static void Remove(std::string& _Text, const std::string& _DeleteText)
     {
-        Replace(_Text, _DeleteText, "", _Text.length() - 1);
+        Replace(_Text, _DeleteText, "", static_cast<int>(_Text.length() - 1));
     }
 };
 
 
 int main()
 {
-    std::string a = "111 2222 33444 455 6";
-    std::string b = "22";
-    std::string c = "777";
-    std::string d = "aAc";
-    //String::Replace(a, b, c, -1);
-    //String::Insert(a, 124241, c);
-    //String::ToUpper(d);
-    String::Remove(a, b);
-    String::TrimRemove(a);
-    std::cout << a << std::endl;
+
+
 }
 
 
